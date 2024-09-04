3cf6202c80546e5ffb7d87149261605a565d048d
sumayaalqadhy <sumaya.alqadhy@gmail.com>
2024-09-04 11:23:30 -0700

    created loop in function body to add items to list

--- ListFunctions.kt
+++ ListFunctions.kt
@@ -1,6 +1,17 @@
 // Write your code below
 fun getListOfNumbers(): List<Int> {
-  
+  var myList = mutableListOf<Int>()
+
+  for (item in 1..7) {
+    println("Enter a value: ")
+    val answer = Integer.valueOf(readLine())
+
+    myList.add(answer)
+  }
+
+  return myList
+
+
 }
 
 
